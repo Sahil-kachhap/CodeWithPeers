a02edb47f6987045947bb9e716b4a26daa8fa979
Sahil <sahil.kachhap111989@gmail.com>
2022-08-03 03:14:58 +0530

    production build

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const {Server} = require('socket.io');
 const ACTIONS = require('./Actions');
 
 
 const server = http.createServer(app); 
 const io = new Server(server);
+
+app.use(express.static('../frontend/build'));
+app.use((req, res, next)=>{
+   res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html'));
+});
+
 const userSocketMap = {};
 
 function getAllConnectedClients(roomId){
